290fc9b4f6acd3cb39ad273de4b42035a08afad4
btkdevkh <amcjkbtk91@outlook.com>
2021-10-23 17:40:45 +0200

    done in this lesson

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
+import { useState } from 'react';
 import logo from './assets/img/logo.svg';
 import './assets/css/App.css';
 
 function App() {
+  // Using the useSate Hook
+  const [name, setName] = useState('Bunthoeun');
+  const [showTechs, setShowTechs] = useState(false);
+  const [techs, setTechs] = useState([
+    { id: 1, techno: "html" },
+    { id: 2, techno: "css" },
+    { id: 3, techno: "javascript" },
+    { id: 4, techno: "php" },
+  ]);
+
+  const handleClick = () => name === '007' ? setName('Bunthoeun') : setName('007');
+  const handleShow = () => setShowTechs((oldState) => !oldState);
+
+  // Using the Previous State
+  const handleDelete = (id) => {
+    setTechs((oldState) => oldState.filter(x => x.id !== id));
+  };
+
+  /* 
+    useState Limitations
+    Always use react hook within the component itself not outside the component
+    React Hooks is not allowed to use in the function except the custom hooks
+  */
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React & Firebase
-        </a>
       </header>
+
+      <div>
+        <h1>My name is {name}</h1>
+        <button onClick={handleClick}>{name === '007' ? 'Hide my nickname' : 'See my nickname'}</button><br />
+        <button onClick={handleShow}>{!showTechs ? 'See my skills' : 'Hide my skills'}</button>
+        <h3>My technical skills are</h3>
+
+        {/* Conditional Templates */}
+        {showTechs && (
+          <ul>
+            {/* Outputting Lists */}
+            {techs.map(tech => (
+              <li 
+                key={tech.id}
+                onClick={() => handleDelete(tech.id)}
+              >
+                {tech.techno.toUpperCase()}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
